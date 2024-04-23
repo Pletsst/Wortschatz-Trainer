19e05d9f9a6df768c7fdb5e4376a57535700d3bb
Pletsst <steven.pletscher@gmx.ch>
2024-04-23 20:01:36 +0200

    modal

--- Wortschatz-Trainer/src/app/erfassen/word-dialog/word-dialog.component.ts
+++ Wortschatz-Trainer/src/app/erfassen/word-dialog/word-dialog.component.ts
@@ -1,12 +1,15 @@
 import { Component, Inject, OnInit } from '@angular/core';
 import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 import { WordPair } from '../../types/word-pair';
+import { MatFormField, MatLabel } from '@angular/material/form-field';
 
 
 @Component({
   selector: 'app-word-dialog',
   templateUrl: './word-dialog.component.html',
   styleUrls: ['./word-dialog.component.css'],
+  standalone: true,
+  imports: [MatFormField, MatLabel]
 })
 export class WordDialogComponent implements OnInit {
   editedWord: WordPair;
